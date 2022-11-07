4b3e4063090494806f47cfb5d2de70ff0c6887d3
jayanthkrishna <gannerla16038@iiitd.ac.in>
2022-11-07 23:39:00 +0530

    Added models

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jayanthkrishna/ecommerce-golang/routes"
+)
+
 func main() {
+
+	app := fiber.New()
+
+	routes.GetRoutes(app)
+	app.Listen(":8085")
 
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,16 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jayanthkrishna/ecommerce-golang/controllers"
+)
 
 func GetRoutes(app *fiber.App) {
+
+	app.Post("/users/signup", controllers.SignUp())
+	app.Post("/users/login", controllers.Login())
+	app.Post("/admin/addproduct", controllers.ProductViewerAdmin())
+	app.Get("/users/productview", controllers.SearchProduct())
+	app.Get("/users/search", controllers.SearchProductByQuery())
 
 }
